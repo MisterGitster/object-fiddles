595e5018f821c54d5d2630df578504472957a4b5
Josh Butler <JButler@JButler-Mac.local>
2014-09-25 19:36:52 -0600

    second fiddle

--- objectFiddle.js
+++ objectFiddle.js
@@ -9,3 +9,31 @@ var me = {
 alert(me.name);
 
 
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+
+var favoriteThings = {
+    band: "Collective Soul",
+    food: "Wings",
+    person: "Dad",
+    book: "DaVinci Code",
+    movie: "Saving Private Ryan",
+    holiday: "Christmas"
+};
+
+
+//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+
+favoriteThings.car = "Corolla";
+favoriteThings.brand = "Toyota";
+
+
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
+
+
+//Now, alert your favorite person, then alert your favorite book.
+
+alert(favoriteThings.person);
+alert(favoriteThings.book);
